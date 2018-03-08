4dc92ec1872fa03775d4c7eafb57a8f175d7a05a
Colin Walsh <cfw37@oit.rutgers.edu>
2018-03-08 18:59:19 -0500

    Properly displaying collectionView

---
+++ virtualTourist/PhotoAlbumCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoAlbumCollectionViewCell.swift
+//  virtualTourist
+//
+//  Created by Colin Walsh on 3/8/18.
+//  Copyright © 2018 Colin Walsh. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumCollectionViewCell: UICollectionViewCell {
+    
+}
--- virtualTourist/PhotoAlbumViewController.swift
+++ virtualTourist/PhotoAlbumViewController.swift
@@ -7,10 +7,60 @@
 //
 
 import UIKit
+import MapKit
+
 
 class PhotoAlbumViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var toolBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        mapView.delegate = self
+        
     }
  }
+
+extension PhotoAlbumViewController: UICollectionViewDelegate {
+    
+}
+
+extension PhotoAlbumViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoAlbumCollectionViewCell
+        return cell
+    }
+    
+}
+
+extension PhotoAlbumViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.collectionView.frame.width/3.25, height: self.collectionView.frame.height/4.25)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+ 
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 7.5, bottom: 5, right: 7.5)
+    }
+ 
+}
+
+extension PhotoAlbumViewController: MKMapViewDelegate {
+    
+}
