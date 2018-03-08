da61f35e4d522593f039fc9beb665e9989b92a2a
Colin Walsh <cfw37@oit.rutgers.edu>
2018-03-08 16:59:33 -0500

    Added viewControllers and classes

---
+++ virtualTourist/TravelLocationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TravelLocationsViewController.swift
+//  virtualTourist
+//
+//  Created by Colin Walsh on 3/8/18.
+//  Copyright © 2018 Colin Walsh. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
